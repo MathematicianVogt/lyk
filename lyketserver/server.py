754d8578fbff507f9dd6e07b5d48523a3b90435d
MathematicianVogt <rvogt@MathVogt.local>
2016-05-13 19:48:00 -0400

    1

--- lyketserver/server.py
+++ lyketserver/server.py
@@ -14,6 +14,7 @@ class LyketHome(tornado.web.RequestHandler):
         res= {}
         res['title']="dongs"
         res['sum']="sumarrydomgs"
+        loader=template.Loader(os.getcwd())
         self.render(loader.load("index.html").generate(res))
 
 
