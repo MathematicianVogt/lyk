611bbcda5cb9abcbd658d40f0cb2c9e787df1b08
MathematicianVogt <rvogt@MathVogt.local>
2016-05-14 11:06:24 -0400

    1

--- lyketserver/server.py
+++ lyketserver/server.py
@@ -57,7 +57,7 @@ class AccountCreationHandler(tornado.web.RequestHandler):
 class LogoutHandler(tornado.web.RequestHandler):
     def post(self):
         session=SessionManager(self)
-        session.delete(self):
+        session.delete(self)
         self.redirect("http://lyket.com/")
 class LoginHandler(tornado.web.RequestHandler):
     def post(self):
