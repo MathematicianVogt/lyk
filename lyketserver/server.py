10c5295a06d2750a93000a86cfb7965fdbf511d0
MathematicianVogt <rvogt@MathVogt.local>
2016-05-13 20:31:39 -0400

    1

--- lyketserver/server.py
+++ lyketserver/server.py
@@ -11,7 +11,7 @@ class LyketHome(tornado.web.RequestHandler):
     def get(self):
         database = self.settings['db']
         db = pymongo.MongoClient()
-        one=db.lyket.articles.find_one()
+        one=db.lyket.articles.find().limit(1).skip( _rand() * db.lyket.articles.count() )
         loader=template.Loader(os.getcwd())
         source=loader.load("index.html").generate(title=one['title'],sum=one['sum'],url=one['url'])
         self.write(source)
